9c9131a96ada3d72a249d23d25e5b8373b853dc5
Jordan Curd <jordan.curd@uservoice.com>
2019-04-24 22:11:30 -0400

    Quick React + TS + webpack bootstrap

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+  render() {
+    return (<div>
+      <h1>HI MOM</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
